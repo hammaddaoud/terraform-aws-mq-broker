23dc2a66fe0e2413b83c8c965c1d94a2bb827f84
Hammad Daoud <107177035+hammaddaoud@users.noreply.github.com>
2023-06-05 18:46:01 +0500

    Update main.tf added application user again

--- main.tf
+++ main.tf
@@ -9,6 +9,11 @@ locals {
   mq_admin_password_needed = local.mq_admin_user_enabled && length(var.mq_admin_password) == 0
   mq_admin_password        = local.mq_admin_password_needed ? random_password.mq_admin_password[0].result : try(var.mq_admin_password[0], "")
 
+  mq_application_user_needed = local.enabled && length(var.mq_application_user) == 0
+  mq_application_user        = local.mq_application_user_needed ? random_pet.mq_application_user[0].id : try(var.mq_application_user[0], "")
+
+  mq_application_password_needed = local.enabled && length(var.mq_application_password) == 0
+  mq_application_password        = local.mq_application_password_needed ? random_password.mq_application_password[0].result : try(var.mq_application_password[0], "")
 
 }
 
@@ -24,6 +29,18 @@ resource "random_password" "mq_admin_password" {
   special = false
 }
 
+resource "random_pet" "mq_application_user" {
+  count     = local.mq_application_user_needed ? 1 : 0
+  length    = 2
+  separator = "-"
+}
+
+resource "random_password" "mq_application_password" {
+  count   = local.mq_application_password_needed ? 1 : 0
+  length  = 24
+  special = false
+}
+  
 resource "aws_ssm_parameter" "mq_master_username" {
   count       = local.mq_admin_user_enabled ? 1 : 0
   name        = format(var.ssm_parameter_name_format, var.ssm_path, var.mq_admin_user_ssm_parameter_name)
@@ -39,6 +56,27 @@ resource "aws_ssm_parameter" "mq_master_password" {
   name        = format(var.ssm_parameter_name_format, var.ssm_path, var.mq_admin_password_ssm_parameter_name)
   value       = local.mq_admin_password
   description = "MQ Password for the admin user"
+  type        = "SecureString"
+  key_id      = var.kms_ssm_key_arn
+  overwrite   = var.overwrite_ssm_parameter
+  tags        = module.this.tags
+}
+
+resource "aws_ssm_parameter" "mq_application_username" {
+  count       = local.enabled ? 1 : 0
+  name        = format(var.ssm_parameter_name_format, var.ssm_path, var.mq_application_user_ssm_parameter_name)
+  value       = local.mq_application_user
+  description = "AMQ username for the application user"
+  type        = "String"
+  overwrite   = var.overwrite_ssm_parameter
+  tags        = module.this.tags
+}
+
+resource "aws_ssm_parameter" "mq_application_password" {
+  count       = local.enabled ? 1 : 0
+  name        = format(var.ssm_parameter_name_format, var.ssm_path, var.mq_application_password_ssm_parameter_name)
+  value       = local.mq_application_password
+  description = "AMQ password for the application user"
   type        = "SecureString"
   key_id      = var.kms_ssm_key_arn
   overwrite   = var.overwrite_ssm_parameter
@@ -81,5 +119,9 @@ resource "aws_mq_broker" "default" {
       groups         = ["admin"]
       console_access = true
     }
+  }
+  user {
+    username = local.mq_application_user
+    password = local.mq_application_password
   }
 }
